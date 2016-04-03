b08fea30910cb226e1790e3b412105a657425bdf
François Allard <binarmorker@gmail.com>
2016-04-03 13:08:11 -0400

    First commit

---
+++ android/src/ca/kitaiweb/sfstory/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ca.kitaiweb.sfstory;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ca.kitaiweb.sfstory.SFStory;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SFStory(), config);
+	}
+}
---
+++ desktop/src/ca/kitaiweb/sfstory/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ca.kitaiweb.sfstory.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ca.kitaiweb.sfstory.SFStory;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SFStory(), config);
+	}
+}
